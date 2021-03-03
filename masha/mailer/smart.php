fe3283bcb0d7946b6965db4bf9046ba32971b6c0
kzruta <kzruta@gmail.com>
2021-03-03 20:52:06 +0300

    u

--- masha/mailer/smart.php
+++ masha/mailer/smart.php
@@ -18,7 +18,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'kolledzh-2017@mail.ru';                 // Наш логин
-$mail->Password = 'gruppabz12';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
